3550aad220810910e3039eb4fe3843c113986f0e
Valerii Solodkyi <vosolodkiy@gmail.com>
2025-10-03 16:47:40 +0300

    add changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { PeoplePage } from './components/PeoplePage';
-import { PageNotFound } from './components/PageNotFound';
+import { NotFoundPage } from './components/NotFoundPage';
 import { Navbar } from './components/Navbar';
 
 export const App = () => (
@@ -14,7 +14,7 @@ export const App = () => (
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="/people" element={<PeoplePage />} />
           <Route path="/people/:slug" element={<PeoplePage />} />
-          <Route path="*" element={<PageNotFound />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </main>
